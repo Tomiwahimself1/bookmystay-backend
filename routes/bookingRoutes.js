7b7c56b5f9340aea72d32d8aa6bf208a2e05d72e
Tomiwahimself1 <adedinsewoadetomiwa@gmail.com>
2026-07-23 10:17:26 +0100

    fix: align booking route handlers with exported controller functions

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -1,26 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { protect, adminOnly } = require('../middleware/authMiddleware');
 const {
   createBooking,
   getMyBookings,
   getBookingById,
   cancelBooking,
-  getAllBookings,
 } = require('../controllers/bookingController');
+const { protect } = require('../middleware/authMiddleware');
 
-router.route('/')
-  .post(protect, createBooking)
-  .get(protect, adminOnly, getAllBookings);
-
-// Make sure /mybookings comes BEFORE /:id !
-router.route('/mybookings')
-  .get(protect, getMyBookings);
-
-router.route('/:id')
-  .get(protect, getBookingById);
-
-router.route('/:id/cancel')
-  .put(protect, cancelBooking);
+router.post('/', protect, createBooking);
+router.get('/mybookings', protect, getMyBookings);
+router.get('/:id', protect, getBookingById);
+router.put('/:id/cancel', protect, cancelBooking);
 
 module.exports = router;
